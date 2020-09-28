0b38e8e4188c2cac3f1e43fd29787cf964eb5793
scortino <stefanocortinovis17@gmail.com>
2020-09-28 15:23:40 +0200

    Add option to manually set the maximum time before TimeoutError is thrown

--- bboard_downloader.py
+++ bboard_downloader.py
@@ -41,6 +41,7 @@ parser = argparse.ArgumentParser(description="Download recordings from Blackboar
 parser.add_argument("url", metavar="URL", type=str, help="URL of the recording")
 parser.add_argument("dest", metavar="DEST", type=str, nargs="?", default="./", help="Directory where to save recording")
 parser.add_argument("--gui", dest="headless", action="store_false", default=True, help="Uses the GUI version of the browser; mostly for debug purposes")
+parser.add_argument("--maxtime", dest="T", type=int, help="Maximum time allowed for the recording to load before a TimeoutError is thrown")
 args = parser.parse_args()
 
 # TODO: add support to other browsers
@@ -49,17 +50,15 @@ opts.headless = args.headless
 driver = webdriver.Firefox(executable_path="./geckodriver_026", options=opts)
 driver.get(args.url)
 try:
-    recording_title = WebDriverWait(driver, 10).until(
+    recording_title = WebDriverWait(driver, args.T).until(
         EC.presence_of_element_located((By.ID, "recording-name"))
-    ).get_attribute("innerText").replace('/', '-')
-    video_src = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "playback-video-playback-video_html5_api"))
-    ).get_attribute("src")
+    ).get_attribute("innerText").replace("/", "-")
+    video_src = driver.find_element_by_id("playback-video-playback-video_html5_api").get_attribute("src")
 except TimeoutException:
     if is_unauthorized(driver):
         raise WebDriverException("Unauthorized request: the recording URL is likely to have expired")
     else:
-        raise TimeoutException("Can't seem to find the video at the specified URL; run the command with --gui for graphical debugging")
+        raise TimeoutException("Can't seem to find the video at the specified URL; try to manually increase the maximum waiting time or run the command with --gui for graphical debugging")
 finally:
     driver.quit()
 
