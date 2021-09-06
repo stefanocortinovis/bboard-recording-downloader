8a5607e148c879bc392ff4db49b6bd6eb0526038
scortino <stefanocortinovis17@gmail.com>
2021-09-06 15:41:55 +0200

    Wait for user to paste URL

--- bboard_downloader.py
+++ bboard_downloader.py
@@ -40,13 +40,14 @@ def is_unauthorized(driver):
 
 parser = argparse.ArgumentParser(description="Download recordings from Blackboard Collaborate. It requires either Chrome or Firefox to be installed and the associated webdriver to be in the current working directory or at a location specified through the executable_path argument.")
 parser.add_argument("dest", metavar="DEST", type=str, nargs="?", default=os.getcwd(), help="Directory where to save recording; default is current directory")
-parser.add_argument("url", metavar="URL", type=str, help="URL of the recording")
+parser.add_argument("url", metavar="URL", type=str, nargs="?", default=None, help="URL of the recording")
 parser.add_argument("--browser", dest="browser", type=str, choices=["chrome", "firefox"], default="chrome", help="Browser to be used to download the recording; currently supported options are Google Chrome and Mozilla Firefox; default is Chrome")
 parser.add_argument("--executable_path", dest="executable_path", type=str, default="default", help="Path to webdriver executable; default is './chromedriver' for Chrome and './geckodriver' for Firefox")
 parser.add_argument("--maxtime", dest="T", type=int, default=10, help="Maximum time in seconds allowed for the recording to load before a TimeoutError is thrown; default is 10s")
 parser.add_argument("--gui", dest="headless", action="store_false", default=True, help="Uses the GUI version of the browser; mostly for debug purposes")
 args = parser.parse_args()
 
+url = input("Paste the recording URL: ") if args.url is None else args.url
 if args.browser.lower() == "chrome":
     opts = webdriver.ChromeOptions()
     opts.headless = args.headless
@@ -61,7 +62,7 @@ elif args.browser.lower() == "firefox":
     driver = webdriver.Firefox(executable_path=args.executable_path, options=opts)
 else:
     raise ValueError("The only currently supported browsers are Google Chrome and Mozilla Firefox")
-driver.get(args.url)
+driver.get(url)
 try:
     recording_title = WebDriverWait(driver, args.T).until(
         EC.presence_of_element_located((By.ID, "recording-name"))
